ef7d29e04ab20ce2f3938db00edf79004d610ff4
Wilson Weber <sanjabian.ho@gmail.com>
2024-03-30 18:20:16 +0330

    Added New Entity Configurations

---
+++ src/Sample-CleanArchitecture-CQRS.Infrastructure/Configuration/Persistence/Identity/ApplicationUserEntityConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sample_CleanArchitecture_CQRS.Infrastructure.Models.Identity;
+
+namespace Sample_CleanArchitecture_CQRS.Infrastructure.Configuration.Persistence.Identity;
+internal class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder
+            .Property(x => x.FirstName)
+            .IsRequired(false)
+            .HasMaxLength(200);
+
+        builder
+            .Property(x => x.LastName)
+            .IsRequired(false)
+            .HasMaxLength(200);
+    }
+}
